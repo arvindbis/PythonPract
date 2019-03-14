b0252628dc49582474dd9d1ea85a99eb0883c18b
Arvind Kumar <arvindkumar@Arvinds-MacBook-Pro.local>
2019-03-14 21:27:50 +0530

    added project

---
+++ PDemo/src/com/demo/Learning.py
@@ -0,0 +1,13 @@
+'''
+Created on 02-Mar-2019
+
+@author: arvindkumar
+'''
+thisdict =    {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+for x,y in thisdict.items():
+    print(x  , y)
